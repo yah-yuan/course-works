3b1a59fb127a025870290a92f8e5f6840425c95b
yahweh <0505yahweh@gmail.com>
2018-04-25 15:06:33 +0800

    朴素贝叶斯

---
+++ Information Content Secrity/mail/mailfilter.py
@@ -0,0 +1,15 @@
+# 实现一个基于朴素贝叶斯算法的文本分类学习过程
+# 垃圾邮件样本:spam_100.utf8
+# 正常邮件样本:ham_100.utf8
+# 测试样本:test.utf8
+
+def 分词():
+    '''step1,对样本进行分词处理'''
+    pass
+
+def 向量化():
+    '''step2,将每个邮件进行向量化'''
+    pass
+
+def trainNB(trainMatrix, trainCategory):
+    '''训练过程'''
