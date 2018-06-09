5c7ec7458cca1ca2cb5afc0ce60339613e9538e4
yahweh <0505yahweh@gmail.com>
2018-06-09 15:58:53 +0800

    gcc

--- Software-Security/gcc_test.c
+++ Software-Security/gcc_test.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
-int i;
-int i = 0x1000;
+int g_uninit[30000];
+int g_init = 0x1000;
 
 void PrintHex(unsigned char * start, int len) {
     int i;
@@ -46,14 +47,49 @@ void stack_protect() {
     for(i=0;i<16;i++) {
         buff[i] = (char)(16-i);
     }
-    buff[16] = 'A';
+    for(i=0;i<8;i++){
+        buff[0x18+i] = 'A';
+    }
     PrintMap(buff-0x10,buff+0x30);
 }
 
 void aslr_pie(){
+    int pid = getpid();
+    char stack = 'a';
+    void * heap_small = malloc(1);
+    void * heap_large = malloc(0x100000);
+    printf("program pid = %d\n",pid);
+    printf("===============================================\n");
+    printf("globle var uninitial is at %p, bss segment\n",&g_uninit);
+    printf("globle var initialed is at %p, data segment\n",&g_init);
+    printf("this function is at %p, program segment\n",&aslr_pie);
+    printf("stack var is at %p, stack segment\n",&stack);
+    printf("small heap is at %p, heap segment\n",heap_small);
+    printf("large heap is at %p, heap segment\n",heap_large);
+    getchar();
+}
 
+void test_memcpy() {
+    char buff[0x10];
+    char str[0x10] = "hey there";
+    strcpy(buff,str); //20个A,溢出
+}
+
+void NX() {
+    int pid = getpid();
+    printf("program pid = %d\n",pid);
+    getchar();
+}
+void relro(){
+    int pid = getpid();
+    printf("program pid = %d\n",pid);
+    getchar();
 }
 
 int main(){
-    stack_protect();
+    // stack_protect();
+    // aslr_pie();
+    // NX();
+    // test_memcpy();
+    relro();
 }
